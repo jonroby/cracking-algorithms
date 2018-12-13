6132023363d9a2174b2ed59eafb690e5d524e55d
jonroby <joncaroby@gmail.com>
2018-12-13 07:27:31 -0800

    add notes

--- 1-arrays-and-strings/1-3-urlify.js
+++ 1-arrays-and-strings/1-3-urlify.js
@@ -5,19 +5,50 @@
 // perform this operation in place.)
 
 // To ensure that the difficulty of the problem approximates the Java version,
-// a string array is required. Time to solve: 1 hour +.
+// a string array is required. Notice that the num of spaces can be obtained by
+// subtracting the string length by the second argument and dividing by 2.
+
+// For example [a 0 b c 0 d 0 0 0 0], where 0s represent spaces. There are two
+// spaces that occur before the padding of zeroes begins after index 5.
+// This makes sense because for every space character, the algo requires a
+// replacement of three characters : '%', '2', '0'. (However, it will be more
+// useful to not divide by 2.)
+
+// d will need to be written to the last idx. So the loop should begin with
+// len - 1 and move backward.
+
+// i = 5 (len - 1)
+// idx to write to: 9, which is equal to i + numOfSpaces (4).
+//  0 1 2 3 4 5 6 7 8 9
+// [a 0 b c 0 d 0 0 0 0] =>
+// [a 0 b c 0 d 0 0 0 d]
+
+// i = 4
+// idx to write to: 6-8, which is equal to i + numOfSpaces (4).
+// Afterward, numOfSpaces needs to be decremented by 2.
+//  0 1 2 3 4 5 6 7 8 9
+// [a 0 b c 0 d 0 0 0 0] =>
+// [a 0 b c 0 d % 2 0 d]
+
+// i = 3
+// idx to write to: 5, which is equal to i + numOfSpaces (2).
+//  0 1 2 3 4 5 6 7 8 9
+// [a 0 b c 0 d % 2 0 d] =>
+// [a 0 b c 0 c % 2 0 d]
+
+// And so on.
 
 // T: O(n)
 // S: O(1)
 function urlify(strArr, len) {
-  let numOfSpaces = (strArr.length - len) / 2;
+  let numOfSpaces = strArr.length - len;
   let count = 0;
   for (let i = len - 1; i >= 0; i--) {
-    let idx = i + numOfSpaces * 2;
+    let idx = i + numOfSpaces;
     if (strArr[i] !== " ") {
       strArr[idx] = strArr[i];
     } else {
-      numOfSpaces = numOfSpaces - 1;
+      numOfSpaces = numOfSpaces - 2;
       strArr[idx] = "0";
       strArr[idx - 1] = "2";
       strArr[idx - 2] = "%";
--- 1-arrays-and-strings/__tests__/1-3-urlify.test.js
+++ 1-arrays-and-strings/__tests__/1-3-urlify.test.js
@@ -6,4 +6,10 @@ describe("urlify", () => {
     const expected = ['M', 'r', '%', '2', '0', 'J', 'o', 'h', 'n', '%', '2', '0', 'S', 'm', 'i', 't', 'h'];
     expect(result).toEqual(expected);
   });
+
+  test("replaces all spaces with %20", () => {
+    const result = urlify(['A', ' ', 'B', 'C', ' ', 'D', ' ', 'E', ' ', ' ', ' ', ' ', ' ', ' '], 8);
+    const expected = ['A', '%', '2', '0', 'B', 'C', '%', '2', '0', 'D', '%', '2', '0', 'E'];
+    expect(result).toEqual(expected);
+  });
 });
