b94b699dbda49b82caf294d69965cbbff02f948c
jonroby <joncaroby@gmail.com>
2018-12-15 00:01:57 -0800

    purely recursive solution

--- 2-linked-lists/2-2-return-kth-to-last.js
+++ 2-linked-lists/2-2-return-kth-to-last.js
@@ -5,10 +5,10 @@ const LinkedList = require("./linked-list");
 
 // T: O(n)
 // S: O(1)
-function removeKthToLast(lst, k) {
+function returnKthToLast1(lst, k) {
   let n = lst._head;
   let prev = null;
-  let count = lst._length - 1 - k;  
+  let count = lst._length - 1 - k;
   while (count > 0) {
     count -= 1;
     n = n._next;
@@ -16,4 +16,44 @@ function removeKthToLast(lst, k) {
   return n;
 }
 
-module.exports = removeKthToLast;
+// T: O(n)
+// S: O(n)
+// Without using length
+function returnKthToLast2(lst, k) {
+  let ret;
+  let traverse = node => {
+    if (node._next !== null) {
+      traverse(node._next);
+      k -= 1;
+      if (k === 0) {
+        ret = node;
+      }
+    }
+  };
+
+  traverse(lst._head);
+  return ret;
+}
+
+function returnKthToLast3(lst, k) {
+  let traverse = (node, k) => {
+    if (node._next === null) {
+      return k - 1;
+    } else {
+      let res = traverse(node._next, k);
+      if (typeof res === "number") {
+        if (res === 0) {
+          return node;
+        } else {
+          return res - 1;
+        }
+      } else {
+        return res;
+      }
+    }
+  };
+
+  return traverse(lst._head, k);
+}
+
+module.exports = { returnKthToLast1, returnKthToLast2, returnKthToLast3 };
--- 2-linked-lists/__tests__/2-2-return-kth-to-last.test.js
+++ 2-linked-lists/__tests__/2-2-return-kth-to-last.test.js
@@ -1,17 +1,51 @@
 const LinkedList = require("../linked-list");
-const returnKthToLast = require('../2-2-return-kth-to-last');
+const {
+  returnKthToLast1,
+  returnKthToLast2,
+  returnKthToLast3
+} = require("../2-2-return-kth-to-last");
 const listValues = require("../list-values");
 
-describe("returnKthToLast", () => {
-  test("in the beginning of list", () => {
+describe("returnKthToLast1 (uses length property)", () => {
+  test("in the middle of list", () => {
     let l = new LinkedList();
     l.appendToTail(1);
     l.appendToTail(2);
     l.appendToTail(3);
     l.appendToTail(4);
     l.appendToTail(5);
-    const result = returnKthToLast(l, 2)._val;
+    const result = returnKthToLast1(l, 2)._val;
     const expected = 3;
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("returnKthToLast2 (partially recursive)", () => {
+  test("in the middle of list", () => {
+    let l = new LinkedList();
+    l.appendToTail(1);
+    l.appendToTail(2);
+    l.appendToTail(3);
+    l.appendToTail(4);
+    l.appendToTail(5);
+    l.appendToTail(6);
+    const result = returnKthToLast2(l, 2)._val;
+    const expected = 4;
+    expect(result).toEqual(expected);
+  });
+});
+
+describe("returnKthToLast3 (purely recursive)", () => {
+  test("in the middle of list", () => {
+    let l = new LinkedList();
+    l.appendToTail(1);
+    l.appendToTail(2);
+    l.appendToTail(3);
+    l.appendToTail(4);
+    l.appendToTail(5);
+    l.appendToTail(6);
+    const result = returnKthToLast3(l, 2)._val;
+    const expected = 4;
     expect(result).toEqual(expected);
   });
 });
