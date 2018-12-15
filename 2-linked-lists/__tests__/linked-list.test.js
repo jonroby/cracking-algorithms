6845e2195de007a6552814667d837d0c08414179
jonroby <joncaroby@gmail.com>
2018-12-14 22:32:55 -0800

    length tests for linked list

--- 2-linked-lists/__tests__/linked-list.test.js
+++ 2-linked-lists/__tests__/linked-list.test.js
@@ -28,4 +28,31 @@ describe("LinkedList", () => {
     const expected = [1, 3];
     expect(values).toEqual(expected);
   });
+
+  test("stores length", () => {
+    let l = new LinkedList();
+    l.appendToTail(1);
+    l.appendToTail(2);
+    l.appendToTail(3);
+    l.appendToTail(4);
+    const result = listValues(l).length;
+
+    const expected = 4;
+    expect(result).toEqual(expected);
+  });
+
+  test("updates length after item is deleted", () => {
+    let l = new LinkedList();
+    l.appendToTail(1);
+    l.appendToTail(2);
+    l.appendToTail(3);
+    l.appendToTail(4);
+
+    l.deleteNode(2);
+    l.deleteNode(4);
+    const result = listValues(l).length;
+
+    const expected = 2;
+    expect(result).toEqual(expected);
+  });
 });
